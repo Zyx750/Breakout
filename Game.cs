a733a85bdaabdc30c10b36af66a85a0b38908539
Zyx750 <maciej.trzaskacz@gmail.com>
2024-02-24 18:05:04 +0100

    launch ball

--- Game.cs
+++ Game.cs
@@ -2,10 +2,26 @@ using Godot;
 
 public partial class Game : Node
 {
+	Ball ball;
+	Paddle paddle;
+	bool launched = false;
 	public override void _Ready()
 	{
-		GetNode<Ball>("Ball").velocity = new Vector2(0,1).Normalized() * GetNode<Ball>("Ball").speed;
+		ball = GetNode<Ball>("Ball");
+		paddle = GetNode<Paddle>("Paddle");
 	}
+
+    public override void _Process(double delta)
+    {
+		if(!launched) {
+			ball.Position = paddle.Position + new Vector2(0,-40);
+			if(Input.IsActionPressed("launch")) {
+				float input = Input.GetAxis("left", "right");
+				ball.velocity = new Vector2(input, -1).Normalized() * ball.speed;
+				launched = true;
+			}
+		}
+    }
 
 	private void OnBallLost() {
 		GD.Print("Game over");
